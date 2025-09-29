e365da2feafa082f927c5ec8a60919c3ba19ee39
Foxy Nhi <trinitykn@protonmail.com>
2025-09-29 11:49:04 +0700

    Delete ci directory

--- ci/stop-mobile.ps1
+++
@@ -1,6 +0,0 @@
-# Stop Appium
-Get-Process node -ErrorAction SilentlyContinue | Where-Object { $_.Path -like "*\node.exe" } | Stop-Process -Force -ErrorAction SilentlyContinue
-# Stop emulator
-taskkill /IM "qemu-system-x86_64.exe" /F 2>$null | Out-Null
-adb kill-server 2>$null | Out-Null
-Write-Host "Stopped Appium and emulator."
